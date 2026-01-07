0e57b0974d3a7ccbe3e20ccd7b8360011738411f
sanchezcodes <lasg125@gmail.com>
2026-01-07 12:17:50 +0100

    changing the try with an if and raising the exception so if is not found we stop the system

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+try:
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+except:
+    RuntimeError("Gemini API Key not found")
+
 def main():
-    print("Hello from my-first-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        )
+    print("Response: ")
+    print(response.text)
 
 
 if __name__ == "__main__":
