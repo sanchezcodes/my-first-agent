7de72e9c3c1f36c33ac1d28f1d2e56620f02d1cf
sanchezcodes <lasg125@gmail.com>
2026-01-07 13:12:10 +0100

    se utilizo response.usage_metadata para hacer seguimiento de los tokens consumidos

--- main.py
+++ main.py
@@ -20,6 +20,12 @@ def main():
     response = client.models.generate_content(
         model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
         )
+    if response.usage_metadata == None:
+        raise RuntimeError("usage_metadata about response is not available")
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
     print("Response: ")
     print(response.text)
 
