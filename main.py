c99bbbda3da440d6f2d97a71dfea35650c9f4681
sanchezcodes <lasg125@gmail.com>
2026-01-12 11:08:04 +0100

    adding function calls to the model, as well the schema of the functions call for the LLM to understand the function and what to use, add a new system prompt for the LLM to use the toos provided, add the list of tools available module

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+import prompts
+from call_functions import available_functions
 
 load_dotenv()
 
@@ -28,18 +30,24 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=messages)
+        model='gemini-2.5-flash',
+        contents=messages,
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=prompts.system_prompt, temperature=0))
     if response.usage_metadata == None:
         raise RuntimeError("usage_metadata about response is not available")
     
     if args.verbose:
         print(f"User prompt: {args.user_prompt}")
+        print(f"System prompt: {prompts.system_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    print("Response: ")
-    print(response.text)
-
+    if response.function_calls != None:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print("Response: ")
+        print(response.text)
 
 if __name__ == "__main__":
     main()
